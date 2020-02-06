0a99f89cb2399ad7fd41dba03108550de5fb623e
Andrea Diaz Aguiar <2396765d@student.gla.ac.uk>
2020-02-06 12:54:04 +0000

    Done Chapter 5 (except exercises)

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # The key boldmessafe matches to {{ boldmessage }} in the template
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # The - in -likes denotes that we would like to order them in descending order
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
 
     # Return a rendered response to send to the client
     # The first parameter is the template we wish to use
@@ -15,3 +19,19 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Andrea Diaz'}
     return render(request, 'rango/about.html', context=context_dict)
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    
+    return render(request, 'rango/category.html', context=context_dict)
+
