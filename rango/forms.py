0b51d41528fb337f6bde03f67e0c96097247538e
Andrea Diaz Aguiar <2396765d@student.gla.ac.uk>
2020-02-08 15:54:07 +0000

    Finished 9.6

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms 
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.") 
@@ -22,6 +23,7 @@ class PageForm(forms.ModelForm):
         model = Page
         exclude = ('category',)
 
+
 def clean(self):
     cleaned_data = self.cleaned_data
     url = cleaned_data.get('url')
@@ -31,3 +33,18 @@ def clean(self):
         cleaned_data['url'] = url
 
     return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
+
