e3e59b034c1e0cf7d37e5e2becfaa6e2ce9f745c
Andrea Diaz Aguiar <2396765d@student.gla.ac.uk>
2020-02-09 22:04:15 +0000

    Chapter 10 done

--- rango/views.py
+++ rango/views.py
@@ -24,14 +24,19 @@ def index(request):
     context_dict['pages'] = page_list
 
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
 
     response = render(request, 'rango/index.html', context=context_dict)
     return response
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {}
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response
 
 
 def show_category(request, category_name_slug):
@@ -167,12 +172,12 @@ def get_server_side_cookie(request, cookie, default_val=None):
 def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request, 'visits', '1'))
 
-    last_visit_cookie = get_server_side_cookie(, request, 'last_visit', str(datetime.now()))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        request.session['last_visit'] = str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
         request.session['last_visit'] = last_visit_cookie
     
